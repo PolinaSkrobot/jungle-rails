35471980a6753755a8c271a7d7e90d9cba4cbb3a
Polina Skrobot <chi.polly.no@gmail.com>
2021-10-20 00:50:55 +0000

    Add to cart test

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts",  type: :feature, js: true do
+  
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+
+  scenario "They add product to cart" do
+    # ACT
+    visit root_path
+    first('.product').click_on 'Add'
+    # DEBUG / VERIFY
+    sleep(1)
+    save_screenshot('cart.png')
+    within('nav') { expect(page).to have_content('My Cart (1)') }   
+  end
+end
+
